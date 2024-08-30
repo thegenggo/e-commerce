4fc7a622434a73c5a7b0c4f0c0736e92b92b8940
Kisane <kritsanaphong-keang@hotmail.com>
2024-08-30 09:45:39 +0700

    feat: add user entity

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import { $Enums, User } from "@prisma/client";
+import { ApiProperty } from "@nestjs/swagger";
+
+export class UserEntity implements User {
+    @ApiProperty()
+    id: number;
+
+    @ApiProperty()
+    username: string;
+
+    @ApiProperty()
+    password: string;
+
+    @ApiProperty()
+    email: string;
+
+    @ApiProperty()
+    role: $Enums.Role;
+
+    @ApiProperty()
+    createdAt: Date;
+
+    @ApiProperty()
+    updatedAt: Date;
+}
